7ed4c94e877f17ce03680f6e26f4a5f94913176a
dsolis421 <danny@march76.com>
2018-02-07 21:09:41 -0600

    fb work

--- public/javascripts/fbscript.js
+++ public/javascripts/fbscript.js
@@ -12,7 +12,7 @@ function shareToFB(url) {
       method: 'share_open_graph',
       action_type: 'og.likes',
       action_properties: JSON.stringify({
-        object:'https://developers.facebook.com/docs/',
+        object: url,
       })
     }, function(response){
       console.log(response);
